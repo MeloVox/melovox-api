35659b4b98bdbf992ed10aec469d96c0bd8ddaaa
lololokoko452 <mano.raichon@orange.fr>
2024-04-09 14:33:21 +0200

    Reviews: Récupération des 20 dernieres Reviews si pas de type renseigné

--- src/routes/review/getReviews.js
+++ src/routes/review/getReviews.js
@@ -2,24 +2,27 @@ import { Review } from '../../db/sequelize.js';
 
 export default (app) => {
   app.post('/api/getReviews', (req, res) => {
-    const { type, id } = req.body;
+    const { type, id, user } = req.body;
 
+    let findOptions = {
+      limit: 2,
+      order: [['createdAt', 'DESC']]
+    };
+    
     if (id && type) {
-      Review.findAll({
-          where: { [type + 'Id']: id },
-      })
-      .then(reviews => {
-          const message = 'Voici les reviews associé à l\'id et au type choisi.'
-          const averageRating = average(reviews);
-          res.json({ message, reviews: reviews, averageRating: averageRating})
-      })
-      .catch(error => {
-          const message = 'Les critiques n\'ont pas pu être récupérées. Réessayez dans quelques instants.'
-          res.status(500).json({ message, data: error })
-      });
-    } else {
-       console.log('Les 20 dernieres reviews');
+      findOptions.where = { [type + 'Id']: id };
     }
+
+    Review.findAll(findOptions)
+    .then(reviews => {
+        const message = 'Voici les critiques associés à l\élément : ' + (type ?? 'global') + ' renseigné.'
+        const averageRating = average(reviews);
+        res.json({ message, reviews: reviews, averageRating: averageRating})
+    })
+    .catch(error => {
+        const message = 'Les critiques n\'ont pas pu être récupérées. Réessayez dans quelques instants.'
+        res.status(500).json({ message, data: error })
+    });
   })
 
   function average(reviews)
